b3e62f2205d433cd2856277a82b803ebaa8e7bb9
Z. R <zmirza7701@gmail.com>
2022-09-28 19:13:02 -0500

    added code to give input for length of password

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbol = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+
+function generatePassword() {
+  var userInput = window.prompt("How many characters do you want your character to be?")
+
+  var passwordLength = parseInt(userInput)
+
+  if (isNaN(passwordLength)){
+    window.alert("That's not a number.")
+    return
+  } 
+
+  if (passwordLength < 8 || passwordLength > 22){
+    window.alert("Password must be between 8 and 22 characters.")
+    return
+  }
+
+  
+
+}
+
+          
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
