9c2398f225442e476efbc71ea1b42fcfd3d49764
Z. R <zmirza7701@gmail.com>
2022-10-03 14:55:16 -0500

    added if statements for user password choice

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var symbol = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
-
 function generatePassword() {
+
   var userInput = window.prompt("How many characters do you want your character to be?")
 
   var passwordLength = parseInt(userInput)
@@ -16,12 +12,38 @@ function generatePassword() {
     return
   } 
 
-  if (passwordLength < 8 || passwordLength > 22){
+  if (passwordLength < 8 || passwordLength > 22) {
     window.alert("Password must be between 8 and 22 characters.")
     return
   }
 
+  var userWantsNumbers = window.confirm("Would you like to include numbers in your password?")
+  var userWantsSymbols = window.confirm("Would you like to include symbols in your password?")
+  var userWantsLowercase = window.confirm("Would you like to include lowercase letters in your password?")
+  var userWantsUppercase = window.confirm("Would you like to include uppercase letters in your password?")
   
+  var numberList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+  var symbolList = ["!", "@", "#", "$", "%", "^", "&", "*"]
+  var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+  var optionsCart = []
+
+  if (userWantsNumbers === true){
+    optionsCart.push(numberList)
+  }
+
+  if (userWantsSymbols === true){
+    optionsCart.push(symbolList)
+  }
+
+  if (userWantsLowercase === true){
+    optionsCart.push(lowercaseList)
+  }
+
+  if (userWantsUppercase === true){
+    optionsCart.push(uppercaseList)
+  }
 
 }
 
